6d364caf2260a64fe31dfd61b6dcde6125095ae1
Spogooglingo <108035625+Observer-Hubert@users.noreply.github.com>
2025-08-14 11:52:17 -0700

    Exit behavior fixed
    Fixed the exit behavior so the scene can be properly exited if needed, and if there is no scene, the game is exited. Small change to the camera placement to fix an issue with fullscreen.

--- Scripts/default_camera.gd
+++ Scripts/default_camera.gd
@@ -3,4 +3,4 @@ extends Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = get_viewport().size/2	
+	position = Vector2(480,360)/2
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,16 +4,16 @@ extends Control
 @onready var main_ui = $Main_UI
 @onready var main_menu = $Main_UI/Main_Menu
 
-
-var currentScene: Node2D
-var currentUI: Control
+var currentScene: Node2D = null
+var currentUI: Control = null
 
 func _ready():
 	main_menu.find_child("Button").pressed.connect(_loadScene)
 
-func _input(event):
+func _input(_event):
 	if Input.is_action_just_pressed("Pause"):
-		_unloadScene()
+		if not _unloadScene():
+			get_tree().quit()
 
 func _loadScene(scene = "forest_scene", ui = "forest_scene_ui"):
 	var loadedScene: PackedScene = load("res://Scenes/" + scene + ".tscn")
@@ -24,7 +24,11 @@ func _loadScene(scene = "forest_scene", ui = "forest_scene_ui"):
 	main_ui.add_child(currentUI)
 	main_menu.visible = false
 
-func _unloadScene():
-	currentScene.queue_free()
-	currentUI.queue_free()
-	main_menu.visible = true
+func _unloadScene() -> bool:
+	if currentScene != null and currentUI != null:
+		currentScene.queue_free()
+		currentUI.queue_free()
+		main_menu.visible = true
+		return true
+	else:
+		return false
