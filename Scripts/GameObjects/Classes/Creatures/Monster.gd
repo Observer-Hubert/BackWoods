38b6d65092876c1c5b2191e536bd9b7ceffd6f5f
Spogooglingo <108035625+Observer-Hubert@users.noreply.github.com>
2025-09-04 17:01:29 -0700

    Tree and bush structure added
    Trees and bushes can now be placed in scenes

--- Scripts/GameObjects/Classes/Creatures/Monster.gd
+++ Scripts/GameObjects/Classes/Creatures/Monster.gd
@@ -3,6 +3,7 @@ extends Creature
 enum monsterStates{IDLE, STALKING, HUNTING}
 
 func _ready() -> void:
+	desiredPos = to_global(position)
 	navigation_agent.velocity_computed.connect(_set_Vel)
 	navigation_agent.target_reached.connect(_target_Reached)
 	max_awareness.connect(_hunt)
